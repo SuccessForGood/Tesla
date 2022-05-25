c6cde4fc61005024a3cb193bb32f705823338aee
Ismael <ec2-user@ip-172-31-23-153.ec2.internal>
2022-05-25 16:36:00 +0000

    tested and approved by the client

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@ This file is only to deploy files
 deploy java application
 deploy python apps
 deploy in the dev and env
+deployment5 master
+deployment4
+deploy to staging env
+echo good job
+tested and accepted
 
